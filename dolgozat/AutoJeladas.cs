45380180efc88f0ac5619a4017d9018da5b87534
Dongesz <majormate12@gmail.com>
2025-02-24 10:43:52 +0100

    1.Feladat - Adatok beolvasása

---
+++ dolgozat/AutoJeladas.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+
+namespace dolgozat
+{
+    public partial class MainWindow : Window
+    {
+        private List<AutoJeladas> jeladasok = new List<AutoJeladas>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnBeolvas_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // Fájl beolvasása
+                string[] sorok = File.ReadAllLines("C:\\Users\\Tanulo\\source\\repos\\dolgozat\\dolgozat\\jeladas.txt");
+
+                // Adatok feldolgozása
+                jeladasok = sorok.Select(sor => new AutoJeladas(sor)).ToList();
+
+                // Kiírás ListBoxba
+                LstAdatok.ItemsSource = jeladasok;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hiba történt a fájl beolvasásakor: " + ex.Message);
+            }
+        }
+    }
+}
--- dolgozat/MainWindow.xaml.cs
+++ dolgozat/MainWindow.xaml.cs
@@ -1,28 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace dolgozat
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    internal class AutoJeladas
     {
-        public MainWindow()
+        public string Rendszam { get; set; }
+        public int Ora { get; set; }
+        public int Perc { get; set; }
+        public int Sebesseg { get; set; }
+
+        public AutoJeladas(string sor)
         {
-            InitializeComponent();
+            var adatok = sor.Split('\t'); // Tabulátor alapú feldolgozás
+            Rendszam = adatok[0];
+            Ora = int.Parse(adatok[1]);
+            Perc = int.Parse(adatok[2]);
+            Sebesseg = int.Parse(adatok[3]);
+        }
+
+        public override string ToString()
+        {
+            return $"{Rendszam} {Ora}:{Perc} - {Sebesseg} km/h";
         }
     }
 }
